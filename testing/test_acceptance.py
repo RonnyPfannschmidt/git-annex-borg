9d48b06ad8b6f5871f329096e1fdb8a518ef3056
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2018-06-29 17:34:48 +0200

    pass borg passwords via env (so we dont log them in the annex branch)

--- testing/test_acceptance.py
+++ testing/test_acceptance.py
@@ -1,4 +1,5 @@
 import subprocess
+import pytest
 
 DATA = b"\x00" * 1000
 
@@ -8,21 +9,26 @@ def do(cmd):
     subprocess.check_call(cmd, shell=isinstance(cmd, str), stderr=subprocess.STDOUT)
 
 
+@pytest.fixture(autouse=True)
+def borg_env(monkeypatch):
+    monkeypatch.setenv("BORG_PASSPHRASE", "example")
+
+
 def test_basic_flow(tmpdir):
     tmpdir.chdir()
     borg_repo = tmpdir.join("borg")
     tmpdir.ensure("data/initial.bin").write_binary(DATA)
-    do(f"BORG_PASSPHRASE=example borg init {borg_repo} -e authenticated")
-    do(f"BORG_PASSPHRASE=example borg create {borg_repo}::data data")
+    do(f"borg init {borg_repo} -e authenticated")
+    do(f"borg create {borg_repo}::data data")
     do("git init annex")
     tmpdir.join("annex").chdir()
     do("git annex init")
     do(
         f"git annex initremote foo type=external externaltype=borg "
-        f"encryption=none repo={borg_repo} passphrase=example "
+        f"encryption=none repo={borg_repo} "
         f"exporttree=no"
     )
     output = subprocess.getoutput("git annex info foo")
     print(output)
     # todo - get infos
-    # assert f"repo: {borg_repo}" in output
+    assert f"borg repo: {borg_repo}" in output
